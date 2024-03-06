04067dca63216fbb0777040b36f4c31ae470063d
Guilherme Lorenzon Tempesta <guilhermetempesta@gmail.com>
2024-03-05 21:16:47 -0300

    correção em commons.js para implementar a função currentDate

--- src/utils/commons.js
+++ src/utils/commons.js
@@ -15,3 +15,9 @@ exports.initializeSequence = async () => {
     console.error('Erro ao inicializar a sequência:', error);
   }
 };
+
+exports.currentDate = () => {
+  const currentDate = new Date();
+  currentDate.setTime(currentDate.getTime() - 3 * 60 * 60 * 1000);
+  return currentDate;
+};
